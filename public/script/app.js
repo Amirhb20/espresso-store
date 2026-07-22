9ad78fb893e3a234e43fadbafa499bfef219c930
Amirhb <amirbolori267@gmail.com>
2026-07-22 14:51:55 +0330

    feat: add responsive header and hero section

--- public/script/app.js
+++ public/script/app.js
@@ -3,6 +3,10 @@ const searchInput = document.querySelector(".search-input");
 const searchBox = document.querySelector(".search-box");
 const searchBtn = document.querySelector(".search-btn");
 const welcome = document.querySelector(".hero-welcome");
+const navOpenBtn = document.querySelector(".nav-open-icon");
+const navcloseBtn = document.querySelector(".nav-close-icon");
+const overlay = document.querySelector(".overlay");
+const nav = document.querySelector(".nav");
 // search Box open & close
 searchBtn.addEventListener("click", function () {
   searchInput.classList.toggle("search-input--active");
@@ -26,3 +30,15 @@ function typeWriter() {
   }
 }
 window.addEventListener("load", typeWriter);
+
+// open & close menu
+navOpenBtn.addEventListener("click", function () {
+  nav.classList.remove("-left-64");
+  nav.classList.add("left-0");
+  overlay.classList.add("overlay--visible");
+});
+navcloseBtn.addEventListener("click", function () {
+  nav.classList.remove("left-0");
+  nav.classList.add("-left-64");
+  overlay.classList.remove("overlay--visible");
+});
